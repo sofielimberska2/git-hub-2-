51a06f22c7fa6a350902a06c608e0cc3db58f0dd
unknown <sofie.limberska@infis.cz>
2025-03-11 09:31:35 +0100

    tot

---
+++ #limberska.py
@@ -0,0 +1,22 @@
+#limberska
+#Nacteni od uzivatele 
+input("jsem zadej na co se chces zeptat")
+
+#pretypovani 
+int (input("zadej cislo"))
+str(input("sem zadejteotazku"))
+
+#logicke operatory 
+float(input("zadejte cislo"))
+
+#3-podminky 
+pocit=("jak se mas")
+
+
+#podminky 
+if cislo>0:
+    print ("cislo kladne")
+elif cislo>0:
+    print ("cislo je zaporne")
+else: 
+    print ("zadejte jine cislo")
